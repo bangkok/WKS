8a253c94dbd5e9c9f6c6766a7d5e3060a2a0ffb2
k.tsigankov <box4list@ya.ru>
2012-04-23 22:46:16 +0300

    конфиг для дб

--- system/application/config/database.php
+++ system/application/config/database.php
@@ -37,10 +37,17 @@
 $active_group = "default";
 $active_record = TRUE;
 
-$db['default']['hostname'] = "localhost";
-$db['default']['username'] = "bangkok_wks";
-$db['default']['password'] = "ghjuekrf";
-$db['default']['database'] = 'bangkok_wks';
+if (str_replace(array('http://', 'www.'), '', $_SERVER['HTTP_HOST']) == 'wks.loc'){
+    $db['default']['hostname'] = "localhost";
+    $db['default']['username'] = "root";
+    $db['default']['password'] = "vertrigo";
+    $db['default']['database'] = "wks";
+} else {
+	$db['default']['hostname'] = "localhost";
+	$db['default']['username'] = "bangkok_wks";
+	$db['default']['password'] = "ghjuekrf";
+	$db['default']['database'] = 'bangkok_wks';
+}
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
